c6a252a2b9e2e5f54ac34769befdeeab1d8b7abf
Innovative-J <juan.cia05678@gmail.com>
2024-08-11 21:53:44 -0500

    final code, files and reademe

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
+
 # PWA-Text-Editor
+
+## Description
 J.A.T.E is a progressive web application that enables users to create and edit text files online or offline. The application will utilize IndexedDB along with the idb package for data storage, guaranteeing offline functionality and consistent performance across various browsers.
+
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Technologies](#technologies)
+5. [License](#license)
+6. [Contributing](#contributing)
+7. [Tests](#tests)
+8. [Screenshot](#screenshot)
+
+## Installation
+1. Clone the repo to your local storage using git clone anf this link https://github.com/Innovative-J/PWA-Text-Editor.git
+
+## Usage
+To utilize this PWA application follow these simple steps
+1. Git clone the repo
+2. Npm install all packages within the client and server side
+3. Build the client by using the command npm run build
+4. Start the server by using npm start and navigate to http://localhost:3000 within your browser
+
+## Technologies
+- HTML
+- CSS
+- JavaScript
+- Node.js
+- Express.js
+- Webpack
+- Babel
+- Workbox
+- IndexedDB
+-PWA
+
+## License
+This project is licensed under the  license.
+
+## Contributing
+Juan G. [Innovative-J]
+
+## Tests
+N/A
+
+## Questions
+For questions about this project, please contact me.
+ [Innovative-J]
+
+## Screenshot
+![Alt text](client\dist\assets\icons\images\Screenshot 2024-08-11 213732.png)
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,41 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// method that accepts some content and adds it to the database
+export const putDb = async (content) =>{
+  console.log('PUT to the database');
+  try {
+    const db = await initdb(); 
+    const tx = db.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({ id: 1, value: content });
+    const result = await request;
+    console.log('Data saved to the database', result.value);
+  } catch (error) {
+    console.error('Failed to add/update content:', error);
+  }
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// method that gets all the content from the database
+export const getDb = async () => {
+  console.log('GET from the database');
+  try {
+    const db = await initdb(); 
+    const tx = db.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.get(1);
+    const result = await request;
+    if (result) {
+      console.log('Data retrieved from the database', result.value);
+      return result.value;
+    } else {
+      console.log('Data not found in the database');
+      return null;
+    }
+  } catch (error) {
+    console.error('Failed to get content:', error);
+    throw error; 
+  }
+};
 
 initdb();
